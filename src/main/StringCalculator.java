468bf9413af1ae32819ffcbafd5bafca6a999a09
opilane <opilane@HP-CZC6297B8V.tthk.ee>
2021-10-04 09:58:06 +0300

    Req1 - add two numbers - tests

---
+++ src/main/StringCalculator.java
@@ -0,0 +1,7 @@
+package main;
+
+public class StringCalculator {
+	public int Add(String numbers) {
+		return 0;
+	}
+}
---
+++ src/test/StringCalculatorTests.java
@@ -0,0 +1,42 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import main.StringCalculator;
+
+class StringCalculatorTests {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void ShouldReturn0ForEmptyString() {
+		StringCalculator sc = new StringCalculator();
+		
+		assertEquals(0, sc.Add(""));
+	}
+	
+	@Test
+	void ShouldReturn1ForString1() {
+		StringCalculator sc = new StringCalculator();
+		
+		assertEquals(1, sc.Add("1"));
+	}
+	
+	@Test
+	void ShouldRetur3ForString_21() {
+		StringCalculator sc = new StringCalculator();
+		
+		assertEquals(3, sc.Add("1,2"));
+	}
+
+}
